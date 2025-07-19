7ced574acb77920178d80b12486a8b1e3878623f
Devyansh Upadhyay <dupadhyay7887@gmail.com>
2025-07-20 01:35:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern 1/pattern-1.java
@@ -0,0 +1,12 @@
+class Solution {
+
+    void printSquare(int n) {
+        int i= 1;
+        while(i<=n){
+            System.out.println("* ".repeat(n));
+            i++;
+        }
+        
+        
+    }
+}
