9a79ea1484fdabbb8c33fe24f02d9bb9439bd993
Devyansh Upadhyay <dupadhyay7887@gmail.com>
2025-07-20 01:44:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Input/Output/java-input-output.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Solution {
+    static void printIntString(String S, int N) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("The input string :"+S);
+        System.out.println("The input integer :"+N);
+        
+    }
+}
