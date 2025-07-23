45139ae4ce4e963327e1eb1d6eca17bf308aa2b5
Devyansh Upadhyay <dupadhyay7887@gmail.com>
2025-07-24 00:13:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern 11/pattern-11.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    void printTriangle(int n) {
+        for(int i = 1;i<=n;i++){
+            int start = (i%2 == 0)? 0:1;//newly used ternary operator
+            for(int j = 1;j<=i;j++){
+                System.out.print(start + " ");
+                start = 1 - start;    
+            }
+            System.out.println();
+        }
+    }
+}
