9be5a96fb15c754e1b37036a1d7547fe5709a358
Devyansh Upadhyay <dupadhyay7887@gmail.com>
2025-08-04 04:31:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
@@ -1,65 +1,53 @@
-import java.util.ArrayList;
-//Imp Queston
-//Do again
 class Solution {
     public static ArrayList<Integer> findUnion(int a[], int b[]) {
-        int i = 0, j = 0; //Initialising two variables tp iterate
-        int n = a.length, m = b.length;
-        ArrayList<Integer> result = new ArrayList<>();
-        Integer lastAdded = null;
-        while (i < n && j < m) {
-            // Skip duplicates in a
-            while (i > 0 && i < n && a[i] == a[i - 1]) {
+        int i = 0, j = 0;
+        int c1 = a.length, c2 = b.length;
+        Integer last = null; //must be Integer as int can not take null as a value
+        ArrayList<Integer> list = new ArrayList<>();
+        while(i < c1 && j < c2){
+            //skipping duplicates from a[]
+            while(i < c1 && i > 0 && a[i] == a[i-1]) i++;
+            //skipping duplicates from b[]
+            while(j < c2 && j > 0 && b[j] == b[j-1]) j++;
+            if(i>=c1 || j>=c2) break;
+            if(a[i] == b[j]){
+                if(last == null || !last.equals(a[i])){
+                    list.add(a[i]);
+                    last = a[i];
+                }
                 i++;
-            }
-            // Skip duplicates in b
-            while (j > 0 && j < m && b[j] == b[j - 1]) {
                 j++;
-            }
-            if (i >= n || j >= m) {
-                break;
-            }
-
-            if (a[i] < b[j]) {
-                if (lastAdded == null || !lastAdded.equals(a[i])) {
-                    result.add(a[i]);
-                    lastAdded = a[i];
+            }else if(a[i] < b[j]){
+                if(last == null || !last.equals(a[i])){
+                    list.add(a[i]);
+                    last = a[i];
                 }
                 i++;
-            } else if (b[j] < a[i]) {
-                if (lastAdded == null || !lastAdded.equals(b[j])) {
-                    result.add(b[j]);
-                    lastAdded = b[j];
+            }else{
+                //a[i] > b[j]
+                if(last == null || !last.equals(b[j])){
+                    list.add(b[j]);
+                    last = b[j];
                 }
-                j++;
-            } else { // a[i] == b[j]
-                if (lastAdded == null || !lastAdded.equals(a[i])) {
-                    result.add(a[i]);
-                    lastAdded = a[i];
-                }
-                i++;
                 j++;
             }
         }
-
-        // Add remaining elements of a
-        while (i < n) {
-            if ((i == 0 || a[i] != a[i - 1]) && (lastAdded == null || !lastAdded.equals(a[i]))) {
-                result.add(a[i]);
-                lastAdded = a[i];
+        //Entering the rest elements of a[] 
+        while(i < c1){
+            if((i == 0 || a[i] != a[i-1])&&(last == null || !last.equals(a[i]))){
+                list.add(a[i]);
+                last = a[i];
             }
             i++;
         }
-
-        // Add remaining elements of b
-        while (j < m) {
-            if ((j == 0 || b[j] != b[j - 1]) && (lastAdded == null || !lastAdded.equals(b[j]))) {
-                result.add(b[j]);
-                lastAdded = b[j];
+        //Entering the rest elements of b[]
+        while(j < c2){
+            if((j == 0 || b[j] != b[j-1])&&(last == null || !last.equals(b[j]))){
+                list.add(b[j]);
+                last = b[j];
             }
             j++;
         }
-
-        return result;
+        return list;
     }
 }
