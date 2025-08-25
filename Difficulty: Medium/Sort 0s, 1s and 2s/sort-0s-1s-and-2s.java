26046834392475b858cef93cf1a5b6ad271dcea3
Devyansh Upadhyay <dupadhyay7887@gmail.com>
2025-08-26 05:08:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void sort012(int[] arr) {
+        // <----------DUTCH NATIONAL FLAG ALGORITHM--------->
+        int n = arr.length;
+        int low = 0, mid = 0, high = n-1;
+        while(mid <= high){
+            if(arr[mid] == 0){
+                int temp = arr[mid];
+                arr[mid] = arr[low];
+                arr[low] = temp;
+                low++;
+                mid++;
+            }else if(arr[mid] == 2){
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }else{
+                mid++;
+            }
+        }
+    }
+}
