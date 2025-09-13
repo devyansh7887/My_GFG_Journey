50e8885e5fad88d43341b6f4e2e64f93d3dce5ab
Devyansh Upadhyay <dupadhyay7887@gmail.com>
2025-09-13 11:09:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Largest number with given sum/largest-number-with-given-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    static String largestNumber(int n, int sum) {
+        // Edge case: sum too large or too small
+        if (sum > 9 * n) return "-1"; // can't distribute sum into n digits
+        if (sum < 0) return "-1";
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            // max digit we can place here
+            int d = Math.min(9, sum);
+            sb.append(d);
+            sum -= d;
+        }
+
+        // If after filling all digits, sum is not 0, then it's impossible
+        if (sum > 0) return "-1";
+
+        return sb.toString();
+    }
+}
